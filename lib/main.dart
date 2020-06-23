f3538bf9c327f29b97d85180c773b31df81c6275
SCGGB <scg.gb.ti@gmail.com>
2020-06-23 15:19:01 -0500

    Se agrego header de app Emergency-Conac

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:headers/src/pages/pinterest_page.dart';
+import 'package:headers/src/pages/emergency_page.dart';
 
  
 void main() => runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Headers App',
-      home: PinterestPage()
+      home: EmergencyPage()
     );
   }
 }
